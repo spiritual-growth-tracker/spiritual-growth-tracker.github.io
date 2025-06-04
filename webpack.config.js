bbe44250ae39ca34cfdfd0ac4d665b1b6b9cba07
Chris <chris@rapidrabbit.software>
2025-06-04 13:54:02 -0500

    Copying over the HTML file as is. No tampering with it as other webpack modules might be doing.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
